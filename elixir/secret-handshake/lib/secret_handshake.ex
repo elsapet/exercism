f057e7ae113b1145f23b6c98707f36bc15b80ae9
Elizabeth B <elizabeth@bearer.sh>
2020-08-15 16:20:06 +0200

    solution

--- elixir/secret-handshake/lib/secret_handshake.ex
+++ elixir/secret-handshake/lib/secret_handshake.ex
@@ -15,40 +15,33 @@ defmodule SecretHandshake do
 
   10000 = Reverse the order of the operations in the secret handshake
   """
+
+  @operations ["wink", "double blink", "close your eyes", "jump"]
+
   @spec commands(code :: integer) :: list(String.t())
   def commands(code) do
-    reversed_list = []
+    []
+    |> build_command_list(Bitwise.band(code, 0b1) > 0, 0)
+    |> build_command_list(Bitwise.band(code, 0b10) > 0, 1)
+    |> build_command_list(Bitwise.band(code, 0b100) > 0, 2)
+    |> build_command_list(Bitwise.band(code, 0b1000) > 0, 3)
+    |> handle_list_reversal(Bitwise.band(code, 0b10000) > 0)
+  end
 
-    reversed_list =
-      case Bitwise.band(code, 0b1) > 0 do
-        true -> ["wink" | reversed_list]
-        _ -> reversed_list
-      end
+  defp build_command_list(acc, true, index) do
+    [Enum.at(@operations, index) | acc]
+  end
 
-    reversed_list =
-      case Bitwise.band(code, 0b10) > 0 do
-        true -> ["double blink" | reversed_list]
-        _ -> reversed_list
-      end
+  defp build_command_list(acc, _, _) do
+    acc
+  end
 
-    reversed_list =
-      case Bitwise.band(code, 0b100) > 0 do
-        true -> ["close your eyes" | reversed_list]
-        _ -> reversed_list
-      end
+  defp handle_list_reversal(acc, false) do
+    Enum.reverse(acc)
+  end
 
-    reversed_list =
-      case Bitwise.band(code, 0b1000) > 0 do
-        true -> ["jump" | reversed_list]
-        _ -> reversed_list
-      end
-
-    reversed_list =
-      case Bitwise.band(code, 0b10000) > 0 do
-        false -> Enum.reverse(reversed_list)
-        _ -> reversed_list
-      end
-
-    reversed_list
+  defp handle_list_reversal(acc, _) do
+    # don't reverse
+    acc
   end
 end
