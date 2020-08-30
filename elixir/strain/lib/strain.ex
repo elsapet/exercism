2820149e2a3d4ad20dd3d1d78d2a6b80ce982e3c
Elizabeth B <elizabeth@bearer.sh>
2020-08-30 10:05:03 +0200

    feat: strain solution iteration 1

---
+++ elixir/strain/lib/strain.ex
@@ -0,0 +1,29 @@
+defmodule Strain do
+  @doc """
+  Given a `list` of items and a function `fun`, return the list of items where
+  `fun` returns true.
+
+  Do not use `Enum.filter`.
+  """
+  @spec keep(list :: list(any), fun :: (any -> boolean)) :: list(any)
+  def keep(list, fun) do
+    conditional_comprehension(list, fun, true)
+  end
+
+  @doc """
+  Given a `list` of items and a function `fun`, return the list of items where
+  `fun` returns false.
+
+  Do not use `Enum.reject`.
+  """
+  @spec discard(list :: list(any), fun :: (any -> boolean)) :: list(any)
+  def discard(list, fun) do
+    conditional_comprehension(list, fun, false)
+  end
+
+  defp conditional_comprehension(list, fun, condition) do
+    for item <- list, fun.(item) == condition do
+      item
+    end
+  end
+end
