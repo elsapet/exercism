0177abe51565888ab3554f9dbc36d1ff8f6954ae
Elizabeth B <elizabeth@bearer.sh>
2020-09-12 09:42:07 +0200

    beer-song second iteration

--- elixir/beer-song/lib/beer_song.ex
+++ elixir/beer-song/lib/beer_song.ex
@@ -3,22 +3,21 @@ defmodule BeerSong do
   Get a single verse of the beer song
   """
   @spec verse(integer) :: String.t()
+  def verse(1) do
+    "1 bottle of beer on the wall, 1 bottle of beer.\n" <>
+      "Take it down and pass it around, no more bottles of beer on the wall.\n"
+  end
+
+  def verse(0) do
+    "No more bottles of beer on the wall, no more bottles of beer.\n" <>
+      "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
+  end
+
   def verse(number) do
-    cond do
-      number > 1 ->
-        remaining = if number - 1 > 1, do: "#{number - 1} bottles", else: "1 bottle"
+    remaining = if number - 1 > 1, do: "#{number - 1} bottles", else: "1 bottle"
 
-        "#{number} bottles of beer on the wall, #{number} bottles of beer.\n" <>
-          "Take one down and pass it around, #{remaining} of beer on the wall.\n"
-
-      number > 0 ->
-        "#{number} bottle of beer on the wall, #{number} bottle of beer.\n" <>
-          "Take it down and pass it around, no more bottles of beer on the wall.\n"
-
-      true ->
-        "No more bottles of beer on the wall, no more bottles of beer.\n" <>
-          "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
-    end
+    "#{number} bottles of beer on the wall, #{number} bottles of beer.\n" <>
+      "Take one down and pass it around, #{remaining} of beer on the wall.\n"
   end
 
   @doc """
